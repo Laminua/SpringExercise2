29a28abda79386ec1cffb1466595a8c9cf275aa4
YarKir <yarkirgen@mail.ru>
2023-04-03 15:28:21 +0500

    Ver 1.06 Exceptions

--- src/main/java/org/example/dao/HibernateUserDAO.java
+++ src/main/java/org/example/dao/HibernateUserDAO.java
@@ -3,9 +3,9 @@ package org.example.dao;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.accounts.models.UserProfile;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,61 +20,101 @@ public class HibernateUserDAO implements AccountService {
 
     @Transactional(readOnly = true)
     public List<UserProfile> index() {
-        Session session = sessionFactory.getCurrentSession();
+        List<UserProfile> userProfiles = null;
 
-        return session.createQuery("SELECT u from UserProfile u", UserProfile.class)
-                .getResultList();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            userProfiles = session.createQuery("SELECT u from UserProfile u", UserProfile.class)
+                    .getResultList();
+        } catch (HibernateException e) {
+            log.error("Ошибка при получении списка пользователей из базы данных");
+            e.printStackTrace();
+        }
+        return userProfiles;
     }
 
     @Transactional
     public void addUser(UserProfile profile) {
         Session session = sessionFactory.getCurrentSession();
-        session.save(profile);
+        try {
+            session.save(profile);
+        } catch (HibernateException e) {
+            log.error("Ошибка при добавлении пользователя в базу данных");
+            e.printStackTrace();
+        }
     }
 
     @Transactional(readOnly = true)
     public UserProfile getUserById(int id) {
+        UserProfile userProfile = null;
+
         Session session = sessionFactory.getCurrentSession();
-        return session.get(UserProfile.class, id);
+
+        try {
+            userProfile = session.get(UserProfile.class, id);
+        } catch (HibernateException e) {
+            log.error("Ошибка при получении пользователя по ID из базы данных");
+            e.printStackTrace();
+        }
+        return userProfile;
     }
 
     @Transactional
     public void updateUser(int id, UserProfile profile) {
-        Session session = sessionFactory.getCurrentSession();
-        UserProfile userProfile = session.get(UserProfile.class, id);
+        UserProfile userProfile = null;
 
-        userProfile.setLogin(profile.getLogin());
-        userProfile.setRole(profile.getRole());
-        userProfile.setName(profile.getName());
-        userProfile.setEmail(profile.getEmail());
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            userProfile = session.get(UserProfile.class, id);
+
+            userProfile.setLogin(profile.getLogin());
+            userProfile.setRole(profile.getRole());
+            userProfile.setName(profile.getName());
+            userProfile.setEmail(profile.getEmail());
+        } catch (HibernateException e) {
+            log.error("Ошибка при обновлении пользователя в базе данных");
+            e.printStackTrace();
+        }
     }
 
     @Transactional
     public void deleteUserById(int id) {
         Session session = sessionFactory.getCurrentSession();
-        session.delete(session.get(UserProfile.class, id));
+
+        try {
+            session.delete(session.get(UserProfile.class, id));
+        } catch (HibernateException e) {
+            log.error("Ошибка при удалении пользователя из базы данных");
+            e.printStackTrace();
+        }
     }
 
     @Transactional(readOnly = true)
     public UserProfile getUserByLogin(String login) {
-        UserProfile userProfile;
+        UserProfile userProfile = null;
         Session session = sessionFactory.getCurrentSession();
 
-        Query<UserProfile> query = session.createQuery("from UserProfile where login=:login",
-                UserProfile.class);
-        query.setParameter("login", login);
-        userProfile = query.uniqueResult();
-
+        try {
+            userProfile = session.createQuery("from UserProfile where login=:login",
+                            UserProfile.class)
+                    .setParameter("login", login)
+                    .uniqueResult();
+        } catch (HibernateException e) {
+            log.error("Ошибка при получении пользователя по логину из базы данных");
+            e.printStackTrace();
+        }
         return userProfile;
     }
 
     @Transactional
     public boolean checkIfLoginExists(String login) {
-        UserProfile userProfile;
-        userProfile = getUserByLogin(login);
-        if (userProfile == null) {
-            return false;
+        UserProfile userProfile = null;
+        try {
+            userProfile = getUserByLogin(login);
+        } catch (HibernateException e) {
+            log.error("Ошибка при проверке наличия логина в базе данных");
+            e.printStackTrace();
         }
-        return true;
+        return userProfile != null;
     }
 }
